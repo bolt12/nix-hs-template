98323550ab303e1d328dbdce378dd5ca1dc04906
Armando Santos <armandoifsantos@gmail.com>
2020-06-26 11:19:58 +0100

    Tweaked hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,8 +7,11 @@ projectName={{cookiecutter.projectName}}
 # Run niv
 nix-shell hook.nix --run "summon new $projectName -f .summoner.toml && lorri init && niv init"
 
-#Remove shell.nix created by lorri
+# Remove shell.nix created by lorri
 rm shell.nix
 
-#Create new nix.shell
+# Create new nix.shell
 echo "(import ./default.nix {}).shell" > shell.nix
+
+# Bring project to top-level
+mv $projectName/* . && rm -r $projectName
