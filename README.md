f14694af8852a9d17e96df929dbecc4f738e5d1c
Armando Santos <armandoifsantos@gmail.com>
2020-06-26 10:33:43 +0100

    - Added summoner derivation since the hackage package is broken.
    - Added README.

---
+++ README.md
@@ -0,0 +1,11 @@
+## How to use
+
+Run:
+
+```bash
+nix-shell -p cookiecutter git --run 'cookiecutter gh:bolt12/hs-nix-template'
+```
+
+## Reference
+
+- https://github.com/utdemir/hs-nix-template
---
+++ hooks/summoner.nix
@@ -0,0 +1,33 @@
+let
+  pkgs = import <nixpkgs> {};
+  summon = pkgs.stdenv.mkDerivation {
+    name = "summon-cli-linux";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/kowainik/summoner/releases/download/v2.0.1.1/summon-cli-linux";
+      sha256 = "6fd241abfc834d3c55f915eaf9e2927d01da0474e84fff18ebca6d67d0c0c117";
+    };
+
+    nativeBuildInputs = [
+      pkgs.autoPatchelfHook
+    ];
+
+    buildInputs = [
+      pkgs.git
+      pkgs.gitAndTools.hub
+      pkgs.curl
+      pkgs.gmp
+    ];
+
+    unpackPhase = ''
+      :
+    '';
+
+    installPhase = ''
+      install -m755 -D $src $out/bin/summon
+    '';
+  };
+in
+  pkgs.mkShell {
+    buildInputs = [ summon ];
+  }
